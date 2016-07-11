0506beb2651ea94a4f3cd86227bdd2dc3e823047
Mike Berkman <mike@berkman.co>
2016-07-10 21:59:19 -0400

    cleanup

--- pipygo.py
+++ pipygo.py
@@ -1,33 +1,27 @@
 import cherrypy
 from cherrypy.process.plugins import Daemonizer
-import os
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
-import time
 import atexit
 from classes.car import Car
 
 my_car = Car()
 
-
-#TODO move to car class?
+# TODO move to car class?
 # create a default object, no changes to I2C address or frequency
 mh = Adafruit_MotorHAT(addr=0x60)
 myMotor = mh.getMotor(3)
 
 
-
-
-
 class PiPyGo(object):
     @cherrypy.expose
 
-    #TODO move to car class?
+    # TODO move to car class?
     # recommended for auto-disabling motors on shutdown!
     def turnOffMotors():
-    	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
     atexit.register(turnOffMotors)
 
@@ -44,19 +38,19 @@ class DriveWebService(object):
     def POST(self, motor_direction):
         cherrypy.session['motor_direction'] = motor_direction
 
-        #TODO move to car class?
-        #print "Forward!"
-        #myMotor.run(Adafruit_MotorHAT.FORWARD)
-        #myMotor.setSpeed(255)
+        # TODO move to car class?
+        # print "Forward!"
+        # myMotor.run(Adafruit_MotorHAT.FORWARD)
+        # myMotor.setSpeed(255)
 
-        #print "Backward!"
-        #myMotor.run(Adafruit_MotorHAT.BACKWARD)
-        #myMotor.setSpeed(255)
+        # print "Backward!"
+        # myMotor.run(Adafruit_MotorHAT.BACKWARD)
+        # myMotor.setSpeed(255)
 
-        #print "Release"
-        #myMotor.run(Adafruit_MotorHAT.RELEASE)
+        # print "Release"
+        # myMotor.run(Adafruit_MotorHAT.RELEASE)
 
-        #self.my_car.set_motor_direction = motor_direction
+        # self.my_car.set_motor_direction = motor_direction
         return motor_direction
 
 class SteerWebService(object):
@@ -68,15 +62,15 @@ class SteerWebService(object):
 
     def POST(self, steering_direction):
         cherrypy.session['steering_direction'] = steering_direction
-        #self.my_car.set_steering_direction = steering_direction
+        # self.my_car.set_steering_direction = steering_direction
         return steering_direction
 
 if __name__ == '__main__':
     config_file = "config/app.conf"
     cherrypy.log("Config File: %s" % config_file)
 
-    #d = Daemonizer(cherrypy.engine)
-    #d.subscribe()
+    # d = Daemonizer(cherrypy.engine)
+    # d.subscribe()
 
     webapp = PiPyGo()
     webapp.drive = DriveWebService()
