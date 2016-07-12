85404c2249c1961096503463d0f3f9f1c25ab847
Mike Berkman <mike@berkman.co>
2016-07-12 15:55:21 +0000

    much changes

--- pipygo.py
+++ pipygo.py
@@ -1,50 +1,31 @@
 import cherrypy
 # from cherrypy.process.plugins import Daemonizer
 from classes.car import Car
-import atexit
 
 
 class PiPyGo(object):
+    my_car = Car()
+
     @cherrypy.expose
     def index(self):
         return open('index.html')
 
-
-class DriveWebService(object):
-    exposed = True
-
-    my_car = Car()
-
-    @cherrypy.tools.accept(media='text/plain')
-    def GET(self):
-        return my_car.get_drive_direction
-
-    def POST(self, drive_direction):
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=['POST'])
+    def drive(self, drive_direction):
         self.my_car.set_drive_direction(drive_direction)
-        return drive_direction
+        return self.my_car.get_drive_direction()
 
-
-class SteerWebService(object):
-    exposed = True
-
-    @cherrypy.tools.accept(media='text/plain')
-    def GET(self):
-        pass
-        # my_car.get_steering_direction
-
-    def POST(self, steering_direction):
-        # self.my_car.set_steering_direction(steering_direction)
-        return steering_direction
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=['POST'])
+    def steer(self, steering_direction):
+        self.my_car.set_steering_direction(steering_direction)
+        return self.my_car.get_steering_direction()
 
 if __name__ == '__main__':
-    config_file = "config/app.conf"
-    cherrypy.log("Config File: %s" % config_file)
+    config_file = "config/app2.conf"
 
     # d = Daemonizer(cherrypy.engine)
     # d.subscribe()
 
-    webapp = PiPyGo()
-    webapp.drive = DriveWebService()
-    webapp.steer = SteerWebService()
-
-    cherrypy.quickstart(webapp, '/', config_file)
+    cherrypy.quickstart(PiPyGo(), '/', config_file)
